0f9a10bc0efb38a660ab3b5fb9a8c41f2073ba62
akash1968 <akash.singh670@gmail.com>
2020-12-10 14:38:37 +0530

    [Akash] .Added Code to save updated employee to local storage

--- js/new_home.js
+++ js/new_home.js
@@ -1,9 +1,9 @@
-
 let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
   empPayrollList = getEmployeePayrollDataFromStorage();
   document.querySelector(".emp-count").textContent = empPayrollList.length;
   createInnerHtml();
+  localStorage.removeItem('editEmp');
 });
 
 const getEmployeePayrollDataFromStorage = () => {
